c7cb26c176ca6ae4b8005c420585d7099387a97a
Alexander Spedding <battleprogrammeralex@gmail.com>
2013-03-12 23:13:45 +0000

    Can print a negative score

--- src/com/alexdiru/redleaf/UtilsString.java
+++ src/com/alexdiru/redleaf/UtilsString.java
@@ -6,7 +6,7 @@ public class UtilsString {
 	private static final StringBuilder mStringBuilder = new StringBuilder(100);
 	private static final char[] mChars = new char[100];
 	private static int[] mDigitBuffer = new int[10];
-	private static String[] mDigits = {"0","1","2","3","4","5","6","7","8","9" };
+	private static String[] mDigits = {"0","1","2","3","4","5","6","7","8","9", "-" };
 	
 	public static StringBuilder getStringBuilder() {
 		mStringBuilder.setLength(0);
@@ -15,6 +15,12 @@ public class UtilsString {
 	
 	/** Appends an integer to the string builder without allocation */
 	public static void appendInteger(int i) {
+		
+		if (i < 0) {
+			mStringBuilder.append(mDigits[10]);
+			i *= -1;
+		}
+		
 		if (i == 0) {
 			mStringBuilder.append(mDigits[0]);
 			return;
